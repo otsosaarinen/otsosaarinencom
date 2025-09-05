af007ba1be681e787b82615416c6e1b54742de1a
Otso Saarinen <saarinenotso@gmail.com>
2025-09-05 21:55:11 +0300

    change prettier styling settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import "./App.css";
 
 function App() {
-  return (
-    <>
-      <div className="font-inter h-screen w-screen bg-blue-950">
-        <h1 className="text-9xl font-semibold text-white">Otso Saarinen</h1>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className="font-inter h-screen w-screen bg-blue-950">
+				<h1 className="text-9xl font-semibold text-white">
+					Otso Saarinen
+				</h1>
+			</div>
+		</>
+	);
 }
 
 export default App;
