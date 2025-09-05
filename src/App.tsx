ba9b17fd57ec2b6299d428dbd873c7bb06a3b6c1
Otso Saarinen <saarinenotso@gmail.com>
2025-09-05 19:44:22 +0300

    add inter font & name title

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="font-inter h-screen w-screen bg-blue-950">
+        <h1 className="text-9xl font-semibold text-white">Otso Saarinen</h1>
+      </div>
+    </>
+  );
 }
 
 export default App;
