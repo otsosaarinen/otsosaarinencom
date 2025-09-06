1d5aa4aebe765b305f1e036c3e6a9edd22af101b
Otso Saarinen <saarinenotso@gmail.com>
2025-09-06 09:27:26 +0300

    create component for links

---
+++ src/components/ContactButton.tsx
@@ -0,0 +1,25 @@
+function ContactButton({
+	link_url,
+	icon: Icon,
+}: {
+	link_url: string;
+	icon: React.ElementType;
+}) {
+	return (
+		<>
+			<div className="group relative flex flex-col items-center justify-center overflow-hidden rounded-full">
+				<div
+					className="flex h-13 w-13 items-center justify-center rounded-full bg-blue-950"
+					onClick={() => window.open(link_url, "_blank")}
+				>
+					<Icon size={25} />
+				</div>
+				<div className="absolute -bottom-13 z-0 flex h-13 w-13 items-center justify-center rounded-full bg-blue-900 transition-all duration-150 ease-in group-hover:bottom-0">
+					<Icon size={25} />
+				</div>
+			</div>
+		</>
+	);
+}
+
+export default ContactButton;
