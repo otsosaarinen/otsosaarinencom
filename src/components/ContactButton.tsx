bf3c14a81d2992697ea880416142cdb25dba19ed
Otso Saarinen <saarinenotso@gmail.com>
2025-09-06 09:34:51 +0300

    add icon color to component

--- src/components/ContactButton.tsx
+++ src/components/ContactButton.tsx
@@ -9,13 +9,13 @@ function ContactButton({
 		<>
 			<div className="group relative flex flex-col items-center justify-center overflow-hidden rounded-full">
 				<div
-					className="flex h-13 w-13 items-center justify-center rounded-full bg-blue-950"
+					className="flex h-13 w-13 items-center justify-center rounded-full bg-blue-700"
 					onClick={() => window.open(link_url, "_blank")}
 				>
-					<Icon size={25} />
+					<Icon size={25} color="white" />
 				</div>
-				<div className="absolute -bottom-13 z-0 flex h-13 w-13 items-center justify-center rounded-full bg-blue-900 transition-all duration-150 ease-in group-hover:bottom-0">
-					<Icon size={25} />
+				<div className="absolute -bottom-13 z-0 flex h-13 w-13 items-center justify-center rounded-full bg-blue-800 transition-all duration-150 ease-in group-hover:bottom-0">
+					<Icon size={25} color="white" />
 				</div>
 			</div>
 		</>
