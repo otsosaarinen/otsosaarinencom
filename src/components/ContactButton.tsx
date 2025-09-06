7519ae5f3d84f3b38909c8920b072d87f4b9247d
Otso Saarinen <saarinenotso@gmail.com>
2025-09-06 11:10:37 +0300

    css changes

--- src/components/ContactButton.tsx
+++ src/components/ContactButton.tsx
@@ -7,16 +7,14 @@ function ContactButton({
 }) {
 	return (
 		<>
-			<div className="group relative flex flex-col items-center justify-center overflow-hidden rounded-full">
+			<div className="group relative flex flex-col items-center justify-center overflow-hidden rounded-full hover:cursor-pointer">
 				<div
-					className="flex h-13 w-13 items-center justify-center rounded-full bg-blue-700"
+					className="flex h-13 w-13 items-center justify-center rounded-full bg-blue-600"
 					onClick={() => window.open(link_url, "_blank")}
 				>
-					<Icon size={25} color="white" />
+					<Icon size={25} color="white" className="z-10" />
 				</div>
-				<div className="absolute -bottom-13 z-0 flex h-13 w-13 items-center justify-center rounded-full bg-blue-800 transition-all duration-150 ease-in group-hover:bottom-0">
-					<Icon size={25} color="white" />
-				</div>
+				<div className="absolute -bottom-13 flex h-13 w-13 items-center justify-center rounded-full bg-blue-500 transition-all duration-150 ease-in group-hover:bottom-0"></div>
 			</div>
 		</>
 	);
