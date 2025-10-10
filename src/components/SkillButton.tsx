00482fcb5ad511a5d89554df03c1617061edb306
Otso Saarinen <saarinenotso@gmail.com>
2025-10-10 20:25:19 +0300

    add cursor-pointer animation

--- src/components/SkillButton.tsx
+++ src/components/SkillButton.tsx
@@ -2,7 +2,7 @@ function SkillButton({ text }: { text: string }) {
 	return (
 		<>
 			<div
-				className={`rounded-2xl border-2 border-white px-4 py-1 text-center text-lg text-white transition-all duration-150 ease-in hover:cursor-pointer hover:bg-white hover:font-semibold hover:text-blue-600`}
+				className={`cursor-pointer rounded-2xl border-2 border-white px-4 py-1 text-center text-lg text-white transition-all duration-150 ease-in hover:bg-white hover:text-blue-600`}
 			>
 				{text}
 			</div>
