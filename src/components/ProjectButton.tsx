26f827134c220e1ef9bf169bde7165270fd10725
Otso Saarinen <saarinenotso@gmail.com>
2025-10-16 19:39:02 +0300

    add props to projectbutton

--- src/components/ProjectButton.tsx
+++ src/components/ProjectButton.tsx
@@ -1,5 +1,19 @@
-function ProjectButton() {
-	return <></>;
+function ProjectButton({
+	title,
+	description,
+	tags,
+	github_link,
+}: {
+	title: string;
+	description: string;
+	tags: string;
+	github_link: string;
+}) {
+	return (
+		<>
+			<div></div>
+		</>
+	);
 }
 
 export default ProjectButton;
