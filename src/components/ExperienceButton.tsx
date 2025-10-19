912f93f9c28774d170af183c28da91651060cb11
Otso Saarinen <saarinenotso@gmail.com>
2025-10-19 20:55:35 +0300

    update experiencebutton colors

--- src/components/ExperienceButton.tsx
+++ src/components/ExperienceButton.tsx
@@ -18,10 +18,12 @@ function ExperienceButton({
 			<div className="flex flex-col justify-between">
 				<div className="flex flex-col">
 					<div className="flex w-full flex-row items-center justify-between">
-						<p className="text-sm font-medium">{title}</p>
+						<p className="text-sm font-medium text-green-300">
+							{title}
+						</p>
 						<p className="text-xs italic">{date}</p>
 					</div>
-					<p className="text-sm text-neutral-300">{company}</p>
+					<p className="text-sm text-white">{company}</p>
 				</div>
 				<p className="py-3 text-sm text-neutral-300">{description}</p>
 				<div className="flex flex-row items-center justify-start gap-0.75 text-xs text-neutral-300">
