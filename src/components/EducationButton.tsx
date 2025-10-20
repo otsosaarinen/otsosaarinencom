43940d0fb7704c96c52b3bab3f144c890b3a7684
Otso Saarinen <saarinenotso@gmail.com>
2025-10-20 14:22:47 +0300

    Create EducationButton.tsx

---
+++ src/components/EducationButton.tsx
@@ -0,0 +1,48 @@
+function EducationButton({
+	title,
+	school,
+	date,
+	description,
+	skills,
+}: {
+	title: string;
+	school: string;
+	date: string;
+	description: string;
+	skills: string[];
+}) {
+	return (
+		<>
+			<div className="group flex cursor-pointer flex-col justify-between px-1 py-3 text-sm transition-colors duration-250 ease-in-out hover:bg-neutral-800">
+				<div className="flex flex-col">
+					<div className="flex w-full flex-row items-center justify-between">
+						<p className="font-medium text-red-300">{title}</p>
+						<p className="text-xs text-neutral-400 italic group-hover:text-white">
+							{date}
+						</p>
+					</div>
+					<p className="text-neutral-400 group-hover:text-white">
+						{school}
+					</p>
+				</div>
+				<p className="my-3 text-neutral-400 group-hover:text-white">
+					{description}
+				</p>
+
+				<div className="flex flex-wrap gap-1.5 text-xs">
+					{skills.map((skill, index) => (
+						<span
+							key={index}
+							className="rounded-sm border-1 border-neutral-400 px-1.5 py-0.5 text-neutral-400 group-hover:border-red-300 group-hover:text-white"
+						>
+							{skill}
+						</span>
+					))}
+				</div>
+			</div>
+			<div className="h-[0.5px] w-full bg-neutral-700"></div>
+		</>
+	);
+}
+
+export default EducationButton;
