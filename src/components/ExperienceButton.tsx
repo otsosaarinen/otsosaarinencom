5df0db3e9c5526062bc50063c877869c06b47f07
Otso Saarinen <saarinenotso@gmail.com>
2025-10-20 14:22:58 +0300

    major css update

--- src/components/ExperienceButton.tsx
+++ src/components/ExperienceButton.tsx
@@ -1,5 +1,3 @@
-import { Tags } from "lucide-react";
-
 function ExperienceButton({
 	title,
 	company,
@@ -15,26 +13,31 @@ function ExperienceButton({
 }) {
 	return (
 		<>
-			<div className="group flex cursor-pointer flex-col justify-between pt-3 pr-1 pb-1 pl-1 text-sm transition-colors duration-250 ease-in-out hover:bg-neutral-700">
+			<div className="group flex cursor-pointer flex-col justify-between px-1 py-3 text-sm transition-colors duration-250 ease-in-out hover:bg-neutral-800">
 				<div className="flex flex-col">
 					<div className="flex w-full flex-row items-center justify-between">
-						<p className="font-medium text-green-300">{title}</p>
-						<p className="text-xs italic">{date}</p>
+						<p className="font-medium text-blue-300">{title}</p>
+						<p className="text-xs text-neutral-400 italic group-hover:text-white">
+							{date}
+						</p>
 					</div>
-					<p className="">{company}</p>
+					<p className="text-neutral-400 group-hover:text-white">
+						{company}
+					</p>
 				</div>
-				<p className="py-3 text-neutral-400 group-hover:text-white">
+				<p className="my-3 text-neutral-400 group-hover:text-white">
 					{description}
 				</p>
-				<div className="mb-3 flex flex-row items-center justify-start gap-0.75 text-xs text-neutral-400">
-					<Tags size={15} color="#7bf1a8" />
-					<div className="flex flex-wrap">
-						{skills.map((skill, index) => (
-							<span key={index} className="rounded-md px-0.75">
-								#{skill}
-							</span>
-						))}
-					</div>
+
+				<div className="flex flex-wrap gap-1.5 text-xs">
+					{skills.map((skill, index) => (
+						<span
+							key={index}
+							className="rounded-sm border-1 border-neutral-400 px-1.5 py-0.5 text-neutral-400 group-hover:border-blue-300 group-hover:text-white"
+						>
+							{skill}
+						</span>
+					))}
 				</div>
 			</div>
 			<div className="h-[0.5px] w-full bg-neutral-700"></div>
