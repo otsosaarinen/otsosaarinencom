22b3d8af67868739e512a28613354dad3a5b060b
Otso Saarinen <saarinenotso@gmail.com>
2025-10-20 14:23:04 +0300

    css changes

--- src/components/ProjectButton.tsx
+++ src/components/ProjectButton.tsx
@@ -13,24 +13,24 @@ function ProjectButton({
 }) {
 	return (
 		<>
-			<div className="group cursor-pointer pt-3 pr-1 pb-1 pl-1 text-sm transition-colors duration-250 ease-in-out hover:bg-neutral-700">
+			<div className="group cursor-pointer px-1 py-3 text-sm transition-colors duration-250 ease-in-out hover:bg-neutral-800">
 				<div className="flex flex-row items-center justify-between">
-					<div className="font-medium text-green-300">{title}</div>
-					<div className="flex flex-row items-center justify-center gap-1 text-neutral-400 group-hover:text-white">
+					<div className="font-medium text-orange-300">{title}</div>
+					<div className="flex flex-row items-center justify-center gap-1 text-neutral-400 group-hover:text-white hover:underline">
 						<a href={github_link} target="_blank">
 							repository
 						</a>
-						<Github size={15} color="#7bf1a8" />
+						<Github size={15} color="#ffb86a" />
 					</div>
 				</div>
-				<div className="py-3 text-neutral-400 group-hover:text-white">
+				<div className="my-3 text-neutral-400 group-hover:text-white">
 					{description}
 				</div>
-				<div className="mb-3 flex flex-wrap space-x-1.5 text-xs">
+				<div className="flex flex-wrap gap-1.5 text-xs">
 					{tags.map((tag, index) => (
 						<span
 							key={index}
-							className="rounded-sm border-1 border-neutral-400 px-1.5 py-0.5 text-neutral-400 group-hover:border-green-300"
+							className="rounded-sm border-1 border-neutral-400 px-1.5 py-0.5 text-neutral-400 group-hover:border-orange-300 group-hover:text-white"
 						>
 							{tag}
 						</span>
